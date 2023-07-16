4d7bebeaef69eb59a02445ef43c297181c4a044e
Salar <salar.mohtaj@gmail.com>
2023-07-16 21:27:16 +0200

    Initial commit

--- Code/Read_Data.py
+++ Code/Read_Data.py
@@ -6,9 +6,9 @@ with open("../Data/ratings.csv",encoding='cp1252') as f:
 
 print(df.columns)
 
-print(df['MOS_Complexity'].corr(df['MOS_Understandability']))
-print(df['MOS_Complexity'].corr(df['MOS_Lexical_difficulty']))
-print(df['MOS_Understandability'].corr(df['MOS_Lexical_difficulty']))
+print(f"The correlation coefficient between Complexity of Understandability is {df['MOS_Complexity'].corr(df['MOS_Understandability'])}")
+print(f"The correlation coefficient between Complexity of Lexical_difficulty is {df['MOS_Complexity'].corr(df['MOS_Lexical_difficulty'])}")
+print(f"The correlation coefficient between Understandability of Lexical_difficulty is {df['MOS_Understandability'].corr(df['MOS_Lexical_difficulty'])}")
 
 
 
@@ -16,10 +16,9 @@ df = df[['ID', 'Sentence', 'MOS_Complexity', 'MOS_Understandability', 'MOS_Lexic
 print(df.columns)
 
 train, test = train_test_split(df, test_size=0.4, shuffle=True)
-
 test, valid = train_test_split(test, test_size=0.5, shuffle=True)
 
-print(train.shape, valid.shape, test.shape)
+print(f"There are {train.shape[0]}, {valid.shape[0]}, and {test.shape[0]} instances in the train, validation and test sets, respectively.")
 
 train.to_csv("../Data/train.csv", encoding='utf-8', index=False)
 valid.to_csv("../Data/valid.csv", encoding='utf-8', index=False)
